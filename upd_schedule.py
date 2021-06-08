b0cb528facfadf0d4f337ee0b3dc5f6ed4c1f07f
Artur B Hefczyc <1848738+arturhefczyc@users.noreply.github.com>
2021-06-08 13:04:24 -0700

    Changed youtube-dl update interval from 30 minutes to 1 day

--- upd_schedule.py
+++ upd_schedule.py
@@ -5,6 +5,6 @@ def exec_interval():
     result = subprocess.run(["pip", "install", "-U", "youtube-dl"])
     print('exec subprocess 1day', result)
 sched = BlockingScheduler()
-sched.add_job(exec_interval, 'interval', seconds=1440)
+sched.add_job(exec_interval, 'interval', seconds=86400)
 
 sched.start()
